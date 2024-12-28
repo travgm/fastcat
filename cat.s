c2406ab41193ffda4c0e81c6ef59f056cfc3c873
Travis <travis@travgm.org>
2024-12-28 14:22:32 -0700

    adding more comments and refactoring

--- cat.s
+++ cat.s
@@ -4,6 +4,10 @@
 # Author: Travis Montoya (trav@hexproof.sh)
 # The do whatever you like with this file license
 #------------------------------------------------------------------------------
+.title            "cat.s"
+.version          "1"
+
+# ------------ Constants ------------
 .set STDOUT,      1
 .set O_RDONLY,    0
 
@@ -13,8 +17,10 @@
 .set __NR_close,  3
 .set __NR_exit,   60
 
+# Arbitary amount set for reading in the line
 .set READ_BUFFER, 2048
     
+# ------------ Initialized data ------------
     .section .data
 
 error_msg:
@@ -24,42 +30,52 @@ invalid_argv:
 
 .align 8
 in_buffer:
+    # Line amount + 1 for null terminator
     .space READ_BUFFER + 1, 0x0
 
+# ------------ Text ------------
     .globl _start
     .section .text
 
 _start:
+    # %rsp on entry holds argc. Check that we have at least a single argument
+    # which needs to be a file name
     mov  (%rsp), %rbx
-    cmp  $2, %rbx
+    cmp  $2, %rbx                      # We compare to 2 because, argv[0] = program name, argv[1] = first argument
     jne  .L_invalid_argv
 
+    # attempt to open argv[1]
     xor  %rdi, %rdi
     mov  16(%rsp), %rdi
     mov  $O_RDONLY, %rsi
     xor  %rdx, %rdx
     mov  $__NR_open, %rax
     syscall
-    cmp  $0, %rax
+    test  %rax, %rax
     je   .L_open_error
 
     mov  %rax, %r12
-read_loop:
+
+    # Loop each line of the file and print it to STDOUT
+    # %r12 holds handle to the file
+1:
     mov  %r12, %rdi
     lea  in_buffer, %rsi
     mov  $READ_BUFFER, %rdx
     mov  $__NR_read, %rax
     syscall
 
+    # %rax contains the number of bytes read, 0 indicates EOF
     test  %rax, %rax
     jle  .L_program_exit
 
+    # NULL terminate buffer and print to STDOUT
     mov  %rax, %rcx
     movb $0, in_buffer(%rcx)
     lea  in_buffer, %rdi
     call print_str
 
-    jmp  read_loop
+    jmp  1b
 .L_invalid_argv:
     lea  invalid_argv, %rdi
     call print_str
@@ -72,6 +88,7 @@ read_loop:
     mov  $__NR_exit, %rax
     syscall
 
+# ---------- Utility Functions ----------
 print_str:
     # Print a string to STDOUT = 1
     # %rdi holds the address of the string
